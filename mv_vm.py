1a6463e0e009f011da64e168276c7361452ba883
opliyal3 <30455352+opliyal3@users.noreply.github.com>
2020-05-07 17:18:52 +0800

    Create mv_vm.py

---
+++ mv_vm.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: wu hsuan
+# Time: 2020/5/7下午 05:15
+# File: mv_vm.py
+
+import os
+
+
+def move():
+    log = '/root/potvm_for_ubuntu/etc/cowrie/log_backup'
+    file = '/root/potvm_for_ubuntu/etc/cowrie/file_backup'
+    des = '/tmp/backup'
+
+    for i in os.listdir(log):
+        try:
+            os.system(f'mkdir -p {des}/{i}/cowrie')
+            os.system(f'tar zcf {des}/{i}/cowrie/cowrie_log_{i}.tar.gz -C {log}/{i}')
+            os.system(f'rm -rf {log}/{i}')
+            print('log tar to /tmp ok!')
+        except:
+            os.system(f'tar zcf {des}/{i}/cowrie/cowrie_log_{i}.tar.gz -C {log}/{i}')
+            os.system(f'rm -rf {log}/{i}')
+            print('log tar to /tmp ok!')
+
+    for i in os.listdir(file):
+        try:
+            os.system(f'mkdir -p {des}/{i}/cowrie')
+            os.system(f'tar zcf {des}/{i}/cowrie/cowrie_log_{i}.tar.gz -C {file}/{i}')
+            os.system(f'rm -rf {file}/{i}')
+            print('file tar to /tmp ok!')
+        except:
+            os.system(f'tar zcf {des}/{i}/cowrie/cowrie_log_{i}.tar.gz -C {file}/{i}')
+            os.system(f'rm -rf {file}/{i}')
+            print('file tar to /tmp ok!')
+
+if __name__ == "__main__":
+    move()
